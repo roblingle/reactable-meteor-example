49baea9240f96a180e85f82854df6a3167b1a0f4
Rob Lingle <rob@oak.works>
2015-12-08 11:49:21 -0500

    switch to pub/sub and add a filter on customerName

--- PaymentsList.jsx
+++ PaymentsList.jsx
@@ -2,24 +2,35 @@ PaymentsList = React.createClass({
   mixins: [ReactMeteorData],
 
   getMeteorData() {
+    // console.log(`Subscribing with filter "${this.state.filterText}"`);
+    var handle = Meteor.subscribe('payments', this.state.filterText);
+
     return {
-      payments: Payments.find({},{fields:{
-        customerName:1, amount:1, tourName:1, createdAt:1, paymentSource:1
-      }, sort:{ createdAt: -1}}).fetch()
+      // can be used to show loading state
+      paymentsLoading: !handle.ready(),
+      payments: Payments.find().fetch()
+    };
+
+  },
     };
   },
 
-  getPayments() {
-    return this.data.payments;
+  filterTextChanged(e){
+    this.setState({ filterText: e.target.value});
+    // console.log(`changed filterText to ${this.state.filterText} for ${e.target.value}`);
   },
 
   render(){
     var Table = Reactable.Table;
     var Thead = Reactable.Thead;
+
+    tableFilter = <input type="text" onChange={this.filterTextChanged} />;
+
     return(
       <div>
         <Table className="ui celled table" data={this.getPayments()}
           itemsPerPage={25} pageButtonLimit={5}
+        {tableFilter}
           sortable={true} defaultSort={{column: 'createdAt', direction: 'desc'}}
           filterable={['customerName','tourName']}
           columns={[
--- react-table-kt-data.jsx
+++ react-table-kt-data.jsx
@@ -1,8 +1,22 @@
 Payments = new Mongo.Collection("payments");
-Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   Meteor.startup(function () {
     ReactDOM.render(<PaymentsList />, document.getElementById("table-target"));
+  });
+}
+
+if (Meteor.isServer) {
+  console.log('publishing payments');
+  Meteor.publish('payments', (filterText) => {
+    return Payments.find(
+      { customerName: new RegExp(filterText,"g")},
+      {
+        fields:{
+          customerName:1, amount:1, tourName:1, createdAt:1, paymentSource:1
+        },
+        // sort:{ createdAt: -1}
+      }
+    );
   });
 }
